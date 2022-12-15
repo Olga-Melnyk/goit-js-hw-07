aef4c86638a475f821d02ec891402fe254e14d34
Olga-Melnyk <3717129@ukr.net>
2022-12-15 14:46:31 +0200

    adds task-01 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+);
+
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+     <img src="${originalImg}" width="800" height="600">
+  `);
+  instance.show();
+}
