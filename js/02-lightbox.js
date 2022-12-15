fe922e921831b51cf357c1c1bb9a984787eb3759
Olga-Melnyk <3717129@ukr.net>
2022-12-16 01:16:12 +0200

    adds task-02.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li>
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+);
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
